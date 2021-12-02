c37486ce00d2057c82eac05fe1ba913e609f93a2
Derick Hansraj <dhansraj@daynite.local>
2021-12-01 21:28:06 -0500

    favorites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -5,6 +5,8 @@ import CampsiteInfo from "./CampsiteInfoComponent";
 import Constants from "expo-constants";
 import AboutComponent from "./AboutComponent";
 import ContactComponent from "./ContactComponent";
+import Favorites from './FavoritesComponent';
+
 import {
   View,
   Platform,
@@ -33,6 +35,29 @@ const mapDispatchToProps = {
   fetchPromotions,
   fetchPartners,
 };
+
+const FavoritesNavigator = createStackNavigator(
+  {
+      Favorites: { screen: Favorites }
+  },
+  {
+      defaultNavigationOptions: ({navigation}) => ({
+          headerStyle: {
+              backgroundColor: '#5637DD'
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+              color: '#fff'
+          },
+          headerLeft: <Icon
+              name='heart'
+              type='font-awesome'
+              iconStyle={styles.stackIcon}
+              onPress={() => navigation.toggleDrawer()}
+          />
+      })
+  }
+);
 
 const DirectoryNavigator = createStackNavigator(
   {
@@ -214,6 +239,20 @@ const MainNavigator = createDrawerNavigator(
         ),
       },
     },
+  Favorites: {
+      screen: FavoritesNavigator,
+      navigationOptions: {
+          drawerLabel: 'My Favorites',
+          drawerIcon: ({tintColor}) => (
+              <Icon
+                  name='heart'
+                  type='font-awesome'
+                  size={24}
+                  color={tintColor}
+              />
+          )
+      }
+  },
     About: {
       screen: AboutNavigator,
       navigationOptions: {
