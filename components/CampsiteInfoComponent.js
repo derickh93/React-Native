9f900dcaddce2d20acd38251d4c521a7df0f7222
Derick Hansraj <derickhansraj@ymail.com>
2021-12-04 10:13:12 -0500

    Animatable

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -11,7 +11,7 @@ import {
 import { Card, Icon, Rating, Input } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
-import * as Animatable from 'react-native-animatable';
+import * as Animatable from "react-native-animatable";
 import { postFavorite, postComment } from "../redux/ActionCreators";
 
 const mapStateToProps = (state) => {
@@ -47,13 +47,15 @@ function RenderComments({ comments }) {
   };
 
   return (
-    <Card title="Comments">
-      <FlatList
-        data={comments}
-        renderItem={renderCommentItem}
-        keyExtractor={(item) => item.id.toString()}
-      />
-    </Card>
+    <Animatable.View animation="fadeInUp" duration={2000} delay={1000}>
+      <Card title="Comments">
+        <FlatList
+          data={comments}
+          renderItem={renderCommentItem}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      </Card>
+    </Animatable.View>
   );
 }
 
@@ -62,34 +64,36 @@ function RenderCampsite(props) {
 
   if (campsite) {
     return (
-      <Card
-        featuredTitle={campsite.name}
-        image={{ uri: baseUrl + campsite.image }}
-      >
-        <Text style={{ margin: 10 }}>{campsite.description}</Text>
-        <View style={styles.cardRow}>
-          <Icon
-            name={props.favorite ? "heart" : "heart-o"}
-            type="font-awesome"
-            color="#f50"
-            raised
-            reverse
-            onPress={() =>
-              props.favorite
-                ? console.log("Already set as a favorite")
-                : props.markFavorite()
-            }
-          />
-          <Icon
-            name="pencil"
-            type="font-awesome"
-            color="#5637DD"
-            raised
-            reverse
-            onPress={() => props.onShowModal()}
-          />
-        </View>
-      </Card>
+      <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+        <Card
+          featuredTitle={campsite.name}
+          image={{ uri: baseUrl + campsite.image }}
+        >
+          <Text style={{ margin: 10 }}>{campsite.description}</Text>
+          <View style={styles.cardRow}>
+            <Icon
+              name={props.favorite ? "heart" : "heart-o"}
+              type="font-awesome"
+              color="#f50"
+              raised
+              reverse
+              onPress={() =>
+                props.favorite
+                  ? console.log("Already set as a favorite")
+                  : props.markFavorite()
+              }
+            />
+            <Icon
+              name="pencil"
+              type="font-awesome"
+              color="#5637DD"
+              raised
+              reverse
+              onPress={() => props.onShowModal()}
+            />
+          </View>
+        </Card>
+      </Animatable.View>
     );
   }
   return <View />;
--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import { ScrollView, Text, View } from "react-native";
 import { Card } from "react-native-elements";
-import * as Animatable from 'react-native-animatable';
-
+import * as Animatable from "react-native-animatable";
 
 export default class ContactComponent extends Component {
   static navigationOptions = {
@@ -11,9 +10,10 @@ export default class ContactComponent extends Component {
   render() {
     return (
       <ScrollView>
-        <Card title="Contact Information" wrapperStyle={{ margin: 20 }}>
-          <Text>
-            {`
+        <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+          <Card title="Contact Information" wrapperStyle={{ margin: 20 }}>
+            <Text>
+              {`
           1 Nucamp Way
           Seattle, WA 98001 
 
@@ -23,8 +23,9 @@ export default class ContactComponent extends Component {
           Phone: 1-206-555-1234 
 
           Email: campsites@nucamp.co `}
-          </Text>
-        </Card>
+            </Text>
+          </Card>
+        </Animatable.View>
       </ScrollView>
     );
   }
